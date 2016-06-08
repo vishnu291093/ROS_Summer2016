dbd87f8b114f360d2d45a52a6b1a1233228cd581
vishnu291093 <vishnu_291093@hotmail.com>
2016-06-08 18:00:04 -0400

    made the header and cpp file

---
+++ workspace/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/vishnu/Desktop/ROS_Summer2016/workspace/src
+# Build directory: /home/vishnu/Desktop/ROS_Summer2016/workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ workspace/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/vishnu/Desktop/ROS_Summer2016/workspace/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/opt/ros/indigo/lib:/usr/local/cuda-6.5/lib64:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PATH="/opt/ros/indigo/bin:/usr/local/cuda-6.5/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/jvm/java-8-oracle/bin:/usr/lib/jvm/java-8-oracle/db/bin:/usr/lib/jvm/java-8-oracle/jre/bin"
+export PWD="/home/vishnu/Desktop/ROS_Summer2016/workspace/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/vishnu/Desktop/ROS_Summer2016/workspace/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/vishnu/Desktop/ROS_Summer2016/workspace/src:$ROS_PACKAGE_PATH"
